55d6afb0e72d8d3d21efaa2ddf9cdd9cd5c8848a
PandoraLS <lisen_work@163.com>
2019-09-17 00:06:26 +0800

    work for Python基础教程&剑指offer

--- Ch02/03_01_DuplicationInArray.py
+++ Ch02/03_01_DuplicationInArray.py
@@ -3,14 +3,23 @@
 # @Author: Li Sen
 
 # 面试题3：数组中重复的数据
-# 题目一：在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+# 题目一：找出数组中重复的数字
+# 在一个长度为n的数组里的所有数字都在0到n-1的范围内。
 # 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
-# 请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+# 请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，
+# 那么对应的输出是第一个重复的数字2。
+
+# 方法一：对输入数组排序，从投到尾扫描，即可得到重复的元素，时间复杂度O(nlogn)
+# 方法二：利用集合，检测元素是否重复
+# 方法三：以将对应元素放到对应位置的方式重排数组，然后发现重复元素
+
 
 # 以牛客网写法为主
 class Solution:
     # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
     # 函数返回True/False
+
+    # 方法一
     def duplicate1(self, numbers, duplication):
         # write code here
         if numbers == None or len(numbers) <= 1:
@@ -27,6 +36,7 @@ class Solution:
                 return True
         return False
 
+    # 方法二
     def duplicate2(self, numbers, duplication):
         if numbers == None or len(numbers) <= 1:
             return False
@@ -42,6 +52,7 @@ class Solution:
                 return True
         return False
 
+    # 方法三
     def duplicate3(self, numbers, duplication):
         if numbers == None or len(numbers) <= 1:
             return False
@@ -57,7 +68,7 @@ class Solution:
                     return True
                 else:
                     temp = numbers[i]
-                    numbers[i] = numbers[temp] # numbers[temp]这里对应的是书中的第m个数字
+                    numbers[i] = numbers[temp]  # numbers[temp]这里对应的是书中的第m个数字
                     numbers[temp] = temp
         return False
 
