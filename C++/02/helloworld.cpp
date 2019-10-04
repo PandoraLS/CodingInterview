2ec37a4a19f79198c25846e569e1ed1413d478b4
PandoraLS <lisen_work@163.com>
2019-10-05 00:34:47 +0800

    work for c++ helloworld

--- C++/02/helloworld.cpp
+++ C++/02/helloworld.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <stdlib.h>  
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << endl;
     return 0;
 }
