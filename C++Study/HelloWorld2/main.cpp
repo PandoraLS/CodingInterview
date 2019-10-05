6a1c6a58612ac91b481507fc24a5bc33222f1655
lisen_work@163.com <lisen_work@163.com>
2019-10-05 09:59:30 +0800

    work for c++

--- C++Study/HelloWorld2/main.cpp
+++ C++Study/HelloWorld2/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
 
 using namespace std;
-
+inline void keep_window_open() {
+    char ch;
+    cin>>ch;
+}
 int main() {
     cout << "Hello world2!" << endl;
     return 0;
