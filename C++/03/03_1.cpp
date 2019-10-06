4cc4a7ae33688ede33593f991640071ad3c9d856
lisen_work@163.com <lisen_work@163.com>
2019-10-06 21:14:47 +0800

    work for chapter 03

---
+++ C++/03/03_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "input :";
+    string first_name;
+    cin >> first_name;
+    cout << "hello, " << first_name <<"\n";
+    return 0;
+}
---
+++ C++/03/03_3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "enter:";
+    string first_name;
+    int age;
+    cin >> first_name;
+    cin >> age;
+    cout << "hello, " << first_name << "(age " << age << ")\n";
+    return 0;
+}
