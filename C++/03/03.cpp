fd4920955e7c1aad0d1585cce87347ada37e820f
PandoraLS <lisen_work@163.com>
2019-10-06 23:31:08 +0800

    work for C++

---
+++ C++/03/03.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     cout << "enter:";
+//     string first_name;
+//     int age;
+//     cin >> first_name;
+//     cin >> age;
+//     cout << "hello, " << first_name << "(age " << age << ")\n";
+//     return 0;
+// }
+
+// int main(){
+//     cout << "inter: ";
+//     string first;
+//     string second;
+//     cin >> first >> second;
+//     cout << "hello, " << first << " " << second << '\n';
+//     return 0;
+// }
+
+int main()
+{
+    cout << "enter:";
+    string first_name;
+    double age;
+    cin >> first_name;
+    cin >> age;
+    cout << "hello, " << first_name << "(age " << age * 12 << ")\n";
+    return 0;
+}
