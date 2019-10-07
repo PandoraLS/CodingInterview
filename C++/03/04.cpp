6c40c84cf15173ac7ad3daafb780fbe286ad1e21
lisen_work@163.com <lisen_work@163.com>
2019-10-07 11:03:28 +0800

    work for c++

--- C++/03/04.cpp
+++ C++/03/04.cpp
@@ -2,10 +2,26 @@
 #include <cmath>
 using namespace std;
 
+// int main()
+// {
+//     cout << "enter a float value: ";
+//     double n;
+//     cin >> n;
+//     cout << "n == " << n
+//          << "\nn+1 == " << n + 1
+//          << "\nthree times n == " << 3 * n
+//          << "\ntwice n == " << n + n
+//          << "\nn squared == " << n * n
+//          << "\nhalf of n == " << n / 2
+//          << "\nsquare root of n == " << sqrt(n)
+//          << "\n";
+//     return 0;
+// }
+
 int main()
 {
     cout << "enter a float value: ";
-    double n;
+    int n;
     cin >> n;
     cout << "n == " << n
          << "\nn+1 == " << n + 1
---
+++ C++/03/05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string previous = " ";
+    string current;
+    while (cin >> current)
+    {
+        if (previous == current){
+            cout << "repeated word: " << current << "\n";
+        }
+        previous = current;
+    }
+    return 0;
+}
