7263b0e0139ea64a1324886daefd63b029ac0ea7
lisen_work@163.com <lisen_work@163.com>
2019-10-10 14:24:33 +0800

    work for C++

--- C++/03/Ex11.cpp
+++ C++/03/Ex11.cpp
@@ -16,7 +16,7 @@ int main() {
     cout << "How many one dollars do you have?  ";
     cin >> one_dollar;
     double total = pennie + 5 * nickel + 10 * dime + 25 * quarter + 50 * half_dollar + 100 * one_dollar;
-    
+
     cout << "原始版本：\n";
     cout << "You have " << pennie << " pennies."
          << "\nYou have " << nickel << " nickels."
@@ -25,14 +25,14 @@ int main() {
          << "\nYou have " << half_dollar << " half dollars."
          << "\nYou have " << one_dollar << " one dollars."
          << "\nThe value of all of your coins is " << total << " cents.\n";
-    
+
     string pennie_plural, nickel_plural, dime_plural, quarter_plural, half_dollar_plural, one_dollar_plural;
-    pennie_plural = (pennie == 1) ? " pennie." : " pennies.";
-    nickel_plural = (nickel == 1) ? " nickel." : " nickels.";
-    dime_plural = (dime == 1) ? " dime." : " dimes.";
-    quarter_plural = (quarter == 1) ? " quarter." : " quarters.";
+    pennie_plural      = (pennie == 1) ? " pennie." : " pennies.";
+    nickel_plural      = (nickel == 1) ? " nickel." : " nickels.";
+    dime_plural        = (dime == 1) ? " dime." : " dimes.";
+    quarter_plural     = (quarter == 1) ? " quarter." : " quarters.";
     half_dollar_plural = (half_dollar == 1) ? " half_dollar." : " half dollars.";
-    one_dollar_plural = (one_dollar == 1) ? " one_dollar." : " one dollars.";
+    one_dollar_plural  = (one_dollar == 1) ? " one_dollar." : " one dollars.";
     cout << "改进版本：\n";
     cout << "You have " << pennie << pennie_plural
          << "\nYou have " << nickel << nickel_plural
@@ -40,6 +40,6 @@ int main() {
          << "\nYou have " << quarter << quarter_plural
          << "\nYou have " << half_dollar << half_dollar_plural
          << "\nYou have " << one_dollar << one_dollar_plural
-         << "\nThe value of all of your coins is $" << total/100.0 << "\n";
+         << "\nThe value of all of your coins is $" << total / 100.0 << "\n";
     return 0;
 }
