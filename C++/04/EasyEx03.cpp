ae2fe50b02329fe1b7b75ac27b7ef2cae87247b8
lisen_work@163.com <lisen_work@163.com>
2019-10-15 17:55:42 +0800

    work for C++

---
+++ C++/04/EasyEx03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 0, b = 0;
+    while (cin >> a >> b) {
+        if (a == b) {
+            cout << "the number are equal \n";
+        }
+    }
+    return 0;
+}
