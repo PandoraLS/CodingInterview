069ef9b549770a1e7a9058b93b8279f0952175e5
lisen_work@163.com <lisen_work@163.com>
2019-10-20 22:57:32 +0800

    work for C++

---
+++ C++/04/Ex05.cpp
@@ -0,0 +1,10 @@
+#include <ctime>
+#include <iostream>
+using namespace std;
+int main(void) {
+    char operation;
+    double a = 0.0, b = 0.0;
+    cout << "输入操作数与操作符: a operation b: \n";
+    // TODO 
+    return 0;
+}
