6a533743d74a1bc6ce0093c45b8cc3708ece8d1a
lisen_work@163.com <lisen_work@163.com>
2019-10-31 17:51:14 +0800

    work for C++

--- C++/04/Ex16.cpp
+++ C++/04/Ex16.cpp
@@ -1,5 +1,35 @@
+// 参考链接：https://blog.csdn.net/hello_worlden/article/details/47761371
+// 使用结构体来记录众数
+
 #include <bits/stdc++.h>
 using namespace std;
+
+struct choosen {
+    int count;  //出现的次数
+    int value;  //值
+};              //记录某一值出现的次数
+
+int choose(vector<int>& L1) {
+    sort(L1.begin(), L1.end()); //对L1按从小到大进行排序
+    choosen Mode, Temp; // Mode用来保存最终结果，Temp用来存储中间结果
+    Mode.count = 0;
+    Mode.value = 0; //Mode初始化
+    Temp.count = 1; 
+    Temp.value = L1.front(); //Temp初始化，从第一个元素开始
+    for (int i = 0; i < L1.size() - 1; i++) {
+        if (L1.at(i) == L1.at(i + 1)) //若相等则次数加一
+            Temp.count++;
+        else {
+            if (Temp.count > Mode.count)
+                Mode = Temp;
+            Temp.count = 1;
+            Temp.value = L1.at(i + 1);
+        }
+    }
+    if (Temp.count > Mode.count)
+        Mode = Temp;
+    return Mode.value;
+}
 
 int main(void) {
     vector<int> v;
@@ -12,15 +42,11 @@ int main(void) {
     for (int x : v) {
         cout << x << " ";
     }
-    // for (int x : v) {
-    //     if (x > max)
-    //         max = x;
-    //     if (x < min)
-    //         min = x;
-    // }
-
+    min  = v[0];
+    max  = v[v.size() - 1];
+    mode = choose(v);
+    cout << "\n最大值：" << max << "\n"
+         << "最小值：" << min << "\n"
+         << "众数： " << mode << endl;
     return 0;
 }
-
-// https://blog.csdn.net/hello_worlden/article/details/47761371
-// TODO 参考链接，有时间看看
