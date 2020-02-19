123346a38b86a5fa932e04dbf40090bb59c7acca
LS-xiaomi <lisen_work@163.com>
2020-02-19 21:41:05 +0800

    work for leetcode

--- LeetCode/只出现一次的数字.py
+++ LeetCode/只出现一次的数字.py
@@ -2,6 +2,9 @@
 # Author：sen
 # Date：2020/2/18 16:24
 # 编程之美 习题1.5 说明：http://www.voidcn.com/article/p-gasydxbz-es.html
+from functools import reduce
+import math
+
 
 class Solution:
     def singleNumber(self, nums):
@@ -11,7 +14,7 @@ class Solution:
         return res
 
     def twoNumber(self, nums):
-        # 两个死机（备份的不是同一个文件）
+        # 两个死机（备份的不是同一个文件），思路三
         res = 0
         for n in nums:
             res ^= n  # 先全部异或，其结果是这两个只出现一次的数字的异或 
@@ -29,6 +32,22 @@ class Solution:
         num2 = num1 ^ res  # 将(num1=B)与(res=A^B)亦或，得到num2=A
         return num1, num2
 
+    def twoNumber2(self, nums, sum_nums, product_nums):
+        """
+        # 思路四，两个机器同时坏掉（两个ID可以相同也可以不同）
+        :param nums: 输入的list
+        :param sum_nums: 总输入list的求和（包含缺失项）
+        :param product_nums: 总输入list的（包含缺失项）
+        :return: 返回所求的两个坏机器的ID
+        """
+        a = sum_nums - sum(nums)  # a是所有的减去缺失的两个 x + y = a
+        b = product_nums / reduce(lambda x, y: x * y, nums)
+        # b是缺失的两项的积 x*y = b，使用乘积形式可能会导致数过大，可以将b这个换成各个平方之和，依然可以求解出x,y
+        # 联立方程组可以求得num1和num2
+        x = (math.sqrt(a * a - 4 * b) + a) / 2
+        y = b / x
+        return x, y
+
 
 if __name__ == '__main__':
     so = Solution()
@@ -36,3 +55,14 @@ if __name__ == '__main__':
     print(so.singleNumber(nums))
     nums2 = [1, 5, 3, 4, 2, 3, 1, 5, 4, 6]
     print(so.twoNumber(nums2))
+
+    # 思路4 
+    nums2 = [1, 5, 3, 4, 2, 3, 1, 5, 4, 6]  # 2和6各缺失一项
+    nums2_sum_nums = sum(nums2) + 2 + 6
+    nums2_product_nums = reduce(lambda x, y: x * y, nums2) * 2 * 6
+    print(so.twoNumber2(nums2, nums2_sum_nums, nums2_product_nums))
+
+    nums3 = [1, 5, 3, 4, 3, 1, 5, 4]  # 假设缺失的是两个2，nums3中的编号均为正数
+    nums3_sum_nums = sum(nums3) + 2 + 2  # 
+    nums3_product_nums = reduce(lambda x, y: x * y, nums3) * 2 * 2
+    print(so.twoNumber2(nums3, nums3_sum_nums, nums3_product_nums))
