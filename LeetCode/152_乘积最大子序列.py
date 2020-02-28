b0fe3cf4c8938e813f42724caaf427dfcb9a1ed6
LS-xiaomi <lisen_work@163.com>
2020-02-28 18:05:46 +0800

    work for leetcode

--- LeetCode/152_乘积最大子序列.py
+++ LeetCode/152_乘积最大子序列.py
@@ -5,25 +5,22 @@ from functools import reduce
 import operator
 
 
+# 题解：https://leetcode-cn.com/problems/maximum-product-subarray/solution/hua-jie-suan-fa-152-cheng-ji-zui-da-zi-xu-lie-by-g/
 class Solution:
     def maxProduct(self, nums):
-
-        tmp_product = nums[0]
-
-        def dfs(rest, begin):
-            if not rest:
-                return tmp_product
-            sub_nums = []
-            for i in range(begin + 1, len(rest) + 1):
-                multi = reduce(operator.mul, nums[begin, i])
-                
-                if multi > tmp_product:
-                    sub_nums.append(nums[begin, i])
-
-        pass
+        max_res, imax, imin = float('-inf'), 1, 1
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                imax, imin = imin, imax
+            imax = max(imax * nums[i], nums[i])
+            imin = min(imin * nums[i], nums[i])
+            max_res = max(max_res, imax)
+        return max_res
 
 
 if __name__ == '__main__':
     so = Solution()
     nums = [2, 3, -2, 4]
     print(so.maxProduct(nums))
+    nums2 = [2, 3, -2, 4, -2]
+    print(so.maxProduct(nums2))
